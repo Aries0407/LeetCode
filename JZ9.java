9221cdf3392346d6a36d0f95305aecbd9f16448d
Aries <Aries@foxmail.com>
2021-02-28 20:03:23 +0800

    剑指Offer题解

---
+++ JZ9.java
@@ -0,0 +1,31 @@
+package LeetCode.SwordToOffer;
+
+import java.util.Stack;
+
+public class JZ9 {
+
+
+    JZ9() {
+    }
+
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> stackHelp = new Stack<>();
+
+    void appendTail(int value) {
+        while (!stack.empty()) {
+            stackHelp.push(stack.pop());
+        }
+        stackHelp.push(value);
+        while (!stackHelp.empty()) {
+            stack.push(stackHelp.pop());
+        }
+    }
+
+    int deleteHead() {
+        return stack.empty() ? -1 : stack.pop();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
