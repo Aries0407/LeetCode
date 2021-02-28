2f59bff6f0786ac78ae414c90c77426d6d127391
Aries <Aries@foxmail.com>
2021-02-28 20:17:34 +0800

    剑指Offer题解

---
+++ JZ22.java
@@ -0,0 +1,22 @@
+package LeetCode.SwordToOffer;
+
+public class JZ22 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        int count = 0;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (count<k) {
+            fast = fast.next;
+            count++;
+        }
+        while (fast!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
